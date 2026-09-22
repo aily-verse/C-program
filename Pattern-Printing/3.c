a7f850bdfcfa03302b3678518bf4ba6a93115c09
aily-verse <ailysett.29@gmail.com>
2026-09-22 16:35:54 +0530

    Create 3.c

---
+++ Pattern-Printing/3.c
@@ -0,0 +1,16 @@
+/*
+1   2   3   4   
+1   2   3   
+1   2   
+1
+*/
+#include<stdio.h>
+void main(){
+    int i,j;
+    for(i=4;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
