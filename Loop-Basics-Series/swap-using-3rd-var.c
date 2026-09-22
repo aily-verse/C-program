9de48027db9ffa48ea2ce740585ec9638fde83aa
aily-verse <ailysett.29@gmail.com>
2026-09-22 15:44:11 +0530

    Add C program to swap two numbers using a third variable

---
+++ Loop-Basics-Series/swap-using-3rd-var.c
@@ -0,0 +1,9 @@
+//WAP to swap 2 no. by using 3rd variable
+#include<stdio.h>
+void main(){
+    int a=30,b=40,c;
+    c=a;
+    a=b;
+    b=c;
+    printf("First number is now %d\nSecond number is now %d",a,b);
+}
