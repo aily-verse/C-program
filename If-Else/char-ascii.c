b6cf248ade2eea0caf659cacc8d215a6608df9d5
aily-verse <ailysett.29@gmail.com>
2026-09-22 15:04:28 +0530

    Add char-ascii.c to print ASCII value of character
    Implement a program to display ASCII value of a character.

---
+++ If-Else/char-ascii.c
@@ -0,0 +1,8 @@
+//WAP to take character input and print the ASCII value
+#include<stdio.h>
+void main(){
+    char x;
+    printf("Enter the letter = ");
+    scanf("%c",&x);
+    printf("Character = %c ASCII = %d",x,x);
+}
