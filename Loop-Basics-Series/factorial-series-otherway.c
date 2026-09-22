3effcdf6e50c8aaa4f812166c068f3c9aa7191b4
aily-verse <ailysett.29@gmail.com>
2026-09-22 15:58:10 +0530

    Add factorial series calculation in C
    Implement factorial series calculation using a loop.

---
+++ Loop-Basics-Series/factorial-series-otherway.c
@@ -0,0 +1,16 @@
+// 1/1! + 2/2! + 3/3! + 4/4! -  n/n!   ANOTHER WAY
+#include<stdio.h>
+void main()
+{
+    int i,n,f=1;
+    float s=0.0;
+    printf("Enter the Range = ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+        printf("%d/%d! ",i,i);
+        s=s+(float)i/(float)f;
+    }
+    printf(" = %.2f",s);
+}
